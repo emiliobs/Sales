2a315a313dd9955c29b12297cfe448201fe30476
emilio barrera <barrera_emilio@hotmail.com>
2018-08-13 14:39:53 +0100

    ApiService

---
+++ Sales/Sales/Services/ApiServices.cs
@@ -0,0 +1,52 @@
+﻿namespace Sales.Services
+{
+    using Newtonsoft.Json;
+    using Sales.Common.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class ApiServices
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+
+                    return new Response()
+                    {
+                      IsSuccess =false,
+                      Message = answer,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+
+                return  new Response()
+                {
+                   IsSuccess = true,
+                   Message = "Ok",
+                   Result = list,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response()
+                {
+                   IsSuccess=false,
+                   Message = ex.Message,
+
+                };
+                
+            }
+        }
+    }
+}
