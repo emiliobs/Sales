a96db3bea5518a6c82c9ec5245e150831fd8d040
emilio barrera <barrera_emilio@hotmail.com>
2018-09-17 16:55:33 +0100

    Register new user, and validation form text

--- Sales/Sales/ViewModels/RegisterPageViewModel.cs
+++ Sales/Sales/ViewModels/RegisterPageViewModel.cs
@@ -95,11 +95,159 @@
 
         #region Commands
         public ICommand ChangeImageCommand { get => new RelayCommand(ChangeImage); }
+        public ICommand SaveCommand { get => new RelayCommand(Save); }
 
 
         #endregion
 
+
         #region Methods
+
+        private  async void Save()
+        {
+            if (string.IsNullOrEmpty(this.FirstName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.FirstNameError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.LastName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.LastNameError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.EMail))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.EMailError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (!RegexHelper.IsValidEmailAddress(this.EMail))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.EMailError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Phone))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PhoneError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PasswordError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (this.Password.Length < 6)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PasswordError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.PasswordConfirm))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PasswordConfirmError,
+                    Languages.Accept);
+                return;
+            }
+
+
+            if (!this.Password.Equals(this.PasswordConfirm))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PasswordsNoMatch,
+                    Languages.Accept);
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+           await Application.Current.MainPage.Navigation.PopAsync();
+
+            //var connection = await this.apiService.CheckConnection();
+            //if (!connection.IsSuccess)
+            //{
+            //    this.IsRunning = false;
+            //    this.IsEnabled = true;
+            //    await Application.Current.MainPage.DisplayAlert(
+            //        Languages.Error,
+            //        connection.Message,
+            //        Languages.Accept);
+            //    return;
+            //}
+
+            //byte[] imageArray = null;
+            //if (this.file != null)
+            //{
+            //    imageArray = FilesHelper.ReadFully(this.file.GetStream());
+            //}
+
+            //var userRequest = new UserRequest
+            //{
+            //    Address = this.Address,
+            //    EMail = this.EMail,
+            //    FirstName = this.FirstName,
+            //    ImageArray = imageArray,
+            //    LastName = this.LastName,
+            //    Password = this.Password,
+            //};
+
+            //var url = Application.Current.Resources["UrlAPI"].ToString();
+            //var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            //var controller = Application.Current.Resources["UrlUsersController"].ToString();
+            //var response = await this.apiService.Post(url, prefix, controller, userRequest);
+
+            //if (!response.IsSuccess)
+            //{
+            //    this.IsRunning = false;
+            //    this.IsEnabled = true;
+            //    await Application.Current.MainPage.DisplayAlert(
+            //        Languages.Error,
+            //        response.Message,
+            //        Languages.Accept);
+            //    return;
+            //}
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            //await Application.Current.MainPage.DisplayAlert(
+            //    Languages.Confirm,
+            //    Languages.RegisterConfirmation,
+            //    Languages.Accept);
+
+            //await Application.Current.MainPage.Navigation.PopAsync();
+
+        }
+
         private async void ChangeImage()
         {
             await CrossMedia.Current.Initialize();
